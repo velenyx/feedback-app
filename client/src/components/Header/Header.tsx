7869b4b1491749ad852b5a448311688e4536d817
TheHeiseDev <meizuset@gmail.com>
2023-09-05 11:48:05 +0300

    add(component): NavItem

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,24 +1,17 @@
 import styles from "./Header.module.scss";
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import classNames from "classnames";
 import { routePath } from "../../shared/config/routePath";
 import { useAuth } from "../../shared/hooks/useAuth";
-import logo from "../../shared/assets/logo.svg";
-
+import { BurgerMenu } from "../BurgerMenu/BurgerMenu";
+import { NavItem } from "../NavItem/NavItem";
 import { RxEnter as OutIcon } from "react-icons/rx";
 import { IoCreateOutline as CreateIcon } from "react-icons/io5";
 import { BiUser as ProfileIcon } from "react-icons/bi";
+import logo from "../../shared/assets/logo.svg";
 
 export const Header = () => {
   const { isAuth, name } = useAuth();
-  const [burgerToggle, setBurgerToggle] = useState(false);
-
-  const signOut = () => {};
-
-  const handleBurgerMenu = () => {
-    setBurgerToggle((prev) => !prev);
-  };
+  const signOut = () => {}
 
   return (
     <header className={styles.header}>
@@ -28,23 +21,23 @@ export const Header = () => {
             <img src={logo} alt="logo" />
           </Link>
         </div>
+
         <nav className={styles.menu}>
           {isAuth && (
-            <>
-              <div className={styles.navMyFeedback}>
-                <div className={styles.navWrapper}>
-                  <CreateIcon />
-                  <Link to={routePath.PROFILE}>Мои отзывы</Link>
-                </div>
-              </div>
-
-              <div className={styles.navProfile}>
-                <div className={styles.navWrapper}>
-                  <ProfileIcon />
-                  <Link to={routePath.PROFILE}>{name}</Link>
-                </div>
-              </div>
-            </>
+            <ul>
+              <NavItem
+                className={styles.navMyFeedback}
+                title="Мои отзывы"
+                path={routePath.PROFILE}
+                Icon={CreateIcon}
+              />
+              <NavItem
+                className={styles.navProfile}
+                title={name}
+                path={routePath.PROFILE}
+                Icon={ProfileIcon}
+              />
+            </ul>
           )}
 
           {isAuth ? (
@@ -57,86 +50,23 @@ export const Header = () => {
               </div>
             </div>
           ) : (
-            <div className={styles.navAuth}>
-              <div className={styles.navWrapper}>
-                <div className={styles.login}>
-                  <OutIcon />
-                  <Link to={routePath.AUTH}>Войти</Link>
-                </div>
-                или
-                <div className={styles.register}>
-                  <Link to={routePath.REGISTRATION}>зарегистрироваться</Link>
-                </div>
-              </div>
+            <div className={styles.authContainer}>
+              <NavItem
+                className={styles.navAuth}
+                title="Войти"
+                path={routePath.AUTH}
+                Icon={OutIcon}
+              />
+              или
+              <NavItem
+                className={styles.navAuth}
+                title="зарегистрироваться"
+                path={routePath.REGISTRATION}
+              />
             </div>
           )}
         </nav>
-
-        <div
-          className={classNames(styles.headerBurger, { [styles.active]: burgerToggle })}
-          onClick={handleBurgerMenu}
-        >
-          <span></span>
-        </div>
-
-        <nav className={classNames(styles.headerMenu, { [styles.active]: burgerToggle })}>
-          <div className={styles.headerMenuWrapper}>
-            {isAuth ? (
-              <div className={styles.burgerNavAuth}>
-                <div className={styles.navWrapper}>
-                  <OutIcon />
-                  <button onClick={signOut}>Выйти</button>
-                </div>
-              </div>
-            ) : (
-              <div className={styles.burgerNavAuth}>
-                <div className={styles.navWrapper}>
-                  <div className={styles.login}>
-                    <OutIcon />
-                    <Link to={routePath.AUTH}>Войти</Link>
-                  </div>
-                  или
-                  <div className={styles.register}>
-                    <Link to={routePath.REGISTRATION}>зарегистрироваться</Link>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {isAuth && (
-              <div className={styles.profileItem}>
-                <div className={styles.navProfile}>
-                  <div className={styles.navWrapper}>
-                    <ProfileIcon />
-                    <Link to={routePath.PROFILE}>{name}</Link>
-                  </div>
-                </div>
-
-                <div className={styles.navMyFeedback}>
-                  <div className={styles.navWrapper}>
-                    <CreateIcon />
-                    <Link to={routePath.PROFILE}>Мои отзывы</Link>
-                  </div>
-                </div>
-              </div>
-            )}
-
-            <div className={styles.menuLinks}>
-              <Link to={routePath.AGREMEENT}>Пользоательское соглашение</Link>
-              <Link to={routePath.COMMUNNITY_RULES}>Правила сообщества</Link>
-              <Link to={routePath.ABOUT}>О компании</Link>
-            </div>
-
-            <div className={styles.burgerNavButton}>
-              <div className={styles.navWrapper}>
-                <Link to={routePath.ADD_FEEDBACK} className={styles.createFeedback}>
-                  <CreateIcon />
-                  Написать отзыв
-                </Link>
-              </div>
-            </div>
-          </div>
-        </nav>
+        <BurgerMenu />
       </div>
     </header>
   );
