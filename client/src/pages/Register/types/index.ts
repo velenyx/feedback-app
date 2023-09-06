7077449719c8f1eeb6982e29ae13b4f4f8090ea4
TheHeiseDev <meizuset@gmail.com>
2023-09-06 11:30:34 +0300

    add(types): added registration types

--- client/src/pages/Register/types/index.ts
+++ client/src/pages/Register/types/index.ts
@@ -3,24 +3,32 @@ export interface IFormRegistration {
   email: string;
   password: string;
 }
-
 export type IRegisterRequest = IFormRegistration;
 
 export type IRegisterResponse = {
-  user: {
-    id: string;
-    email: string;
-    name: string;
-    role: string;
-  };
-  tokens: {
-    access: {
-      token: string;
-      expires: string;
-    };
-    refresh: {
-      token: string;
-      expires: string;
-    };
-  };
+  user: User;
+  tokens: AuthTokens;
+};
+
+export type User = {
+  id: string;
+  email: string;
+  name: string;
+  role: Role;
+};
+export type AuthTokens = {
+  access: Token;
+  refresh: Token;
+};
+export type Token = {
+  token: string;
+  expires: string;
+};
+export enum Role {
+  admin = "admin",
+  user = "user",
+}
+export type Error = {
+  code: number;
+  message: string;
 };
--- client/src/shared/ui/Buttons/AuthButton.tsx
+++ client/src/shared/ui/Buttons/AuthButton.tsx
@@ -2,14 +2,14 @@ import styles from "./AuthButton.module.scss";
 import classNames from "classnames";
 import { CircularProgress as Loading } from "@mui/material";
 
-interface IAuthButton {
+interface IAuthButtonProps {
   name: string;
   type: "submit" | "button";
   callback?: () => void;
   loading: boolean;
 }
 
-export const AuthButton = ({ name, type, callback, loading }: IAuthButton) => {
+export const AuthButton = ({ name, type, callback, loading }: IAuthButtonProps) => {
   return (
     <button
       type={type}
