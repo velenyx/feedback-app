342fe4b7f3624fdf627e88207f4e66337d57d0f8
TheHeiseDev <meizuset@gmail.com>
2023-09-10 20:04:46 +0300

    add(auth): added authorization

--- client/src/pages/Auth/api/index.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosInstance } from "axios";
-
-const API_URL = "https://feedback-app-backend-wzfj.onrender.com/v1"
-export const $api: AxiosInstance = axios.create({
-  withCredentials: true,
-  baseURL: API_URL,
-  timeout: 1000,
-});
