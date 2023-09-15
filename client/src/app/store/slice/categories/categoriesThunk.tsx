6796055df91f551c3620feedd37a69d31469d70a
TheHeiseDev <meizuset@gmail.com>
2023-09-15 14:44:21 +0300

    fix(styles): svg icon styles fixed

--- client/src/app/store/slice/categories/categoriesThunk.tsx
+++ client/src/app/store/slice/categories/categoriesThunk.tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { CategoriesType } from "./categoriesTypes";
 
 // const BASE_URL = "https://feedback-app-backend-wzfj.onrender.com/v1";
 
 export const fetchCategories = createAsyncThunk(
   "categories/fetchCategories",
   async () => {
-    const { data } = await axios({
+    const { data } = await axios<CategoriesType[]>({
       method: "GET",
       // url: BASE_URL + "/categories",
       url: "https://297c2cda3254946b.mokky.dev/categories",
