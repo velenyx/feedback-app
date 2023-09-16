f3990ea1626848f9a880c3bd36108c7c499fc8d8
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-09-16 20:03:46 +1200

    Fix statuses

--- server/src/controllers/category.controller.js
+++ server/src/controllers/category.controller.js
@@ -4,7 +4,7 @@ const { categoryService } = require('../services');
 
 const getCategories = catchAsync(async (req, res) => {
   const categories = await categoryService.getCategories();
-  res.status(httpStatus.FOUND).send(categories);
+  res.status(httpStatus.OK).send(categories);
 });
 
 const createCategory = catchAsync(async (req, res) => {
