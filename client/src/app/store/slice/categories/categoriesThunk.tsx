8872e95912500b03c3f1762e57a1a6feb548d7c7
TheHeiseDev <meizuset@gmail.com>
2023-09-16 11:44:42 +0300

    feature: added favicon icon

--- client/src/app/store/slice/categories/categoriesThunk.tsx
+++ client/src/app/store/slice/categories/categoriesThunk.tsx
@@ -1,16 +1,14 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { CategoriesType } from "./categoriesTypes";
-
-// const BASE_URL = "https://feedback-app-backend-wzfj.onrender.com/v1";
+import { BASE_URL } from "../../../../shared/config/url";
 
 export const fetchCategories = createAsyncThunk(
   "categories/fetchCategories",
   async () => {
     const { data } = await axios<CategoriesType[]>({
       method: "GET",
-      // url: BASE_URL + "/categories",
-      url: "https://297c2cda3254946b.mokky.dev/categories",
+      url: BASE_URL + "/categories",
     });
     return data;
   }
--- client/src/components/NavItem/NavItem.tsx
+++ client/src/components/NavItem/NavItem.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import { IconType } from "react-icons";
 import styles from "./NavItem.module.scss";
 
-interface INavItem {
+interface INavItemProps {
   className: string;
   path: string;
   title: string;
   Icon?: IconType;
 }
-export const NavItem = ({ className, path, title, Icon }: INavItem) => {
+export const NavItem = ({ className, path, title, Icon }: INavItemProps) => {
   return (
     <li className={className}>
       <div className={styles.navWrapper}>
