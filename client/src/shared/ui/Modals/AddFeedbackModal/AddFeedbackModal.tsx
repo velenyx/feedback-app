42784a33b7ae952a075a15924e5b32f256fd1037
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-09-18 09:27:09 +1200

    Create feedback base UI

--- client/src/shared/ui/Modals/AddFeedbackModal/AddFeedbackModal.tsx
+++ client/src/shared/ui/Modals/AddFeedbackModal/AddFeedbackModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./AddFeedbackModal.module.scss";
+import CreateFeedbackButton from "../../Buttons/CreateFeedbackButton/CreateFeedbackButton";
 type AddFeedbackModalProps = {
   isActive: boolean;
   setVisibility: () => void;
@@ -15,7 +16,18 @@ const AddFeedbackModal: React.FC<AddFeedbackModalProps> = ({
       className={`${styles.modal} ${isActive ? styles.active : ""}`}
     >
       <div onClick={(e) => e.stopPropagation()} className={styles.content}>
-        Content
+        <div className={styles.overflow}>
+          <h3 className={styles.title}>Create feedback</h3>
+          <textarea
+            className={styles.commentInput}
+            name=""
+            id=""
+            cols={30}
+            rows={10}
+            placeholder="What do you think about think client?"
+          ></textarea>
+          <CreateFeedbackButton clickHandler={() => {}} />
+        </div>
       </div>
     </div>
   );
