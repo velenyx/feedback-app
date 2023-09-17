99bbd6b5917a3b625ce814f6fdb152ba2461ef25
TheHeiseDev <meizuset@gmail.com>
2023-09-17 18:27:36 +0300

    fix(widget): fixed icon and color widget

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -28,15 +28,15 @@ export const Home = () => {
                     linkTitle="Правила"
                     link={routePath.COMMUNNITY_RULES}
                     bgUrl="https://cmp.dns-shop.ru/images/78097831b51ec906c029.png"
-                    bgColor="#ffefef"
+                    bgColor="#ecf9ff"
                   />
                   <Widget
                     title="Помощь"
                     description="Частые вопросы, полезная информация"
                     linkTitle="Помощь"
                     link={routePath.ABOUT}
-                    bgUrl="https://cmp.dns-shop.ru/images/755ac8590433f3a6e50e.png"
-                    bgColor="#ecf9ff"
+                    bgUrl="https://cmp.dns-shop.ru/images/c97288867824cbe1ed8d.webp"
+                    bgColor="#ffefef"
                   />
                 </div>
               </div>
