74acf90cabf2277f54302580564c3c17d933c611
TheHeiseDev <meizuset@gmail.com>
2023-09-22 11:13:32 +0300

    initial

--- client/src/pages/Feedback/Feedback.tsx
+++ client/src/pages/Feedback/Feedback.tsx
@@ -1,7 +1,16 @@
 import { useParams } from "react-router-dom";
+import { MainLayout } from "../../layout/MainLayout";
 import styles from "./Feedback.module.scss";
 
 export const Feedback = () => {
   const { id } = useParams();
-  return <div style={styles}>Feedback:  {id}</div>;
+  return (
+    <MainLayout>
+      <div className={styles.feedbackPage}>
+        <div className={styles.container}>
+        Feedback: {id}
+        </div>
+      </div>
+    </MainLayout>
+  );
 };
