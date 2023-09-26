74845039c84e83c4c6895bb78ecb6560a91d1e26
mykhailo-svg <74141693+mykhailo-svg@users.noreply.github.com>
2023-09-26 21:22:10 +0300

    feature:(client) feedback link (#19)
    * Add feedback link UI
    
    * Improve feedback link styles and add rating
    
    * Add mui rating and refactor components splitting
    
    * Add feedback card skeleton
    
    * Add responsive design for feedback card
    
    * Remove skeletons and add active card styles
    
    * Add spans and improve text-hiding for feedback card
    
    * Add alts and improve semantic
    
    * Fix alts
    
    * Fix alts names
    
    * Fix locks
    
    * Resolving conflicts with dev
    
    ---------
    
    Co-authored-by: Mykhailo Vydryk <“wyzdrykm@gmail.com”>

--- client/src/shared/helpers/saveTokens.ts
+++ client/src/shared/helpers/saveTokens.ts
@@ -1,6 +1,6 @@
 import { AuthTokens } from "../../pages/Register/types";
 
-export const saveTokensLocalStoare = (data: AuthTokens): void => {
+export const saveTokensLocalStorage = (data: AuthTokens): void => {
   localStorage.setItem("accessToken", data.access.token);
   localStorage.setItem("refreshToken", data.refresh.token);
 };
