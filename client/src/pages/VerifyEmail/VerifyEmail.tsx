fc4543bfd614f6825d10fd7800689f4e22f688bb
TheHeiseDev <meizuset@gmail.com>
2023-09-27 10:46:13 +0300

    add(lib): added lib qs

---
+++ client/src/pages/VerifyEmail/VerifyEmail.tsx
@@ -0,0 +1,12 @@
+import styles from "./VerifyEmail.module.scss"
+
+export const VerifyEmail = () => {
+  return (
+    <div className={styles.verifyEmail}>
+     <div className={styles.container}>
+     Верификация
+     </div>
+    </div>
+  )
+}
+
