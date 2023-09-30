d259572d579c9e35f974b3b4206e1a52bfe550a0
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-09-30 14:04:06 +1200

    Add testing fedback card

--- client/src/components/MyFeedbacks/MyFeedbacks.tsx
+++ client/src/components/MyFeedbacks/MyFeedbacks.tsx
@@ -1,6 +1,7 @@
 import Pagination from "@mui/material/Pagination";
 import { useState } from "react";
 import styles from "./MyFeedbacks.module.scss";
+import { FeedbackCard } from "../FeedbackCard/FeedbackCard";
 
 export const MyFeedbacks = () => {
   const [page, setPage] = useState(1);
@@ -12,6 +13,18 @@ export const MyFeedbacks = () => {
   };
   return (
     <div>
+      <div className={styles.feedbacksContainer}>
+        <FeedbackCard
+          text="dfg"
+          comments={0}
+          views={1}
+          rating={0}
+          category="df"
+          id="df"
+          name="Vyzdryk Mykhailo"
+  
+        />
+      </div>
       <button className={styles.pagination}>
         <Pagination
           color="secondary"
