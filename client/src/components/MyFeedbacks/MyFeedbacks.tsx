6d7a1eda5d38f5bce2320b94a0771869d1492142
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-09-30 13:42:25 +1200

    Add mui pagination

--- client/src/components/MyFeedbacks/MyFeedbacks.tsx
+++ client/src/components/MyFeedbacks/MyFeedbacks.tsx
@@ -1,3 +1,25 @@
+import Pagination from "@mui/material/Pagination";
+import { useState } from "react";
+import styles from "./MyFeedbacks.module.scss";
+
 export const MyFeedbacks = () => {
-  return <div>MyFeedbacks</div>;
+  const [page, setPage] = useState(1);
+  const handlePaginationChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+  return (
+    <div>
+      <button className={styles.pagination}>
+        <Pagination
+          color="secondary"
+          count={10}
+          page={page}
+          onChange={handlePaginationChange}
+        />
+      </button>
+    </div>
+  );
 };
