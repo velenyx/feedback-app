239fe9f6d583e5026a3f9f12466fe2ca1a911678
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-10-07 12:00:37 +1200

    Add swagger comment and reply schema

--- server/src/routes/v1/comments.route.js
+++ server/src/routes/v1/comments.route.js
@@ -17,3 +17,68 @@ router
   .patch(auth(), commentsController.rateReply);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Comment:
+ *       type: object
+ *       properties:
+ *         creator:
+ *           type: string
+ *           description: The ID of the creator
+ *         targetId:
+ *           type: string
+ *           description: The ID of the target entity
+ *         text:
+ *           type: string
+ *           description: The comment text
+ *         replies_count:
+ *           type: number
+ *           description: Number of replies to the comment
+ *         likes:
+ *           type: number
+ *           description: Number of likes for the comment
+ *         dislikes:
+ *           type: number
+ *           description: Number of dislikes for the comment
+ *         createdDate:
+ *           type: string
+ *           format: date-time
+ *           description: The date the comment was created
+ *       required:
+ *         - creator
+ *         - targetId
+ *         - text
+ *
+ *     Reply:
+ *       type: object
+ *       properties:
+ *         creator:
+ *           type: string
+ *           description: The ID of the creator
+ *         comment:
+ *           type: string
+ *           description: The ID of the parent comment
+ *         reply_to:
+ *           type: string
+ *           description: The ID of the user being replied to (if any)
+ *         text:
+ *           type: string
+ *           description: The reply text
+ *         likes:
+ *           type: number
+ *           description: Number of likes for the reply
+ *         dislikes:
+ *           type: number
+ *           description: Number of dislikes for the reply
+ *         createdDate:
+ *           type: string
+ *           format: date-time
+ *           description: The date the reply was created
+ *       required:
+ *         - creator
+ *         - comment
+ *         - text
+ */
