fe04cbb01a575d5506669ca9c4ffc6edffa3692c
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-10-12 21:20:30 +1200

    improve reply creation validation

--- server/src/validations/comments.validation.js
+++ server/src/validations/comments.validation.js
@@ -2,7 +2,8 @@ const Joi = require('joi');
 
 const createComemntOrReply = {
   body: Joi.object().keys({
-    text: Joi.string().required().min(10)
+    text: Joi.string().required().min(10),
+    reply_to: Joi.allow()
   })
 };
 
